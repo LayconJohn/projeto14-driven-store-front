137e0bfe0721bf5ab298092387023176db0118be
Norival <norivaljunior.jf@gmail.com>
2022-09-14 21:42:43 -0300

    editando globastyle

--- src/assets/globalStyles.js
+++ src/assets/globalStyles.js
@@ -1,14 +1,65 @@
-import styled, {createGlobalStyle} from "styled-components";
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 25px;
-    background: #E5CDC8;
-    font-family: Open-Sans, Helvetica, Sans-Serif;
-    box-sizing: border-box;
-    font-family: 'Raleway', sans-serif;
-  }
-`;
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	
+	vertical-align: baseline;
+}
 
-export {GlobalStyle}
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+  font-family: Open-Sans, Helvetica, Sans-Serif;
+	font-family: 'Raleway', sans-serif;
+background-color: #f4f4f4;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+strong {
+	font-weight: bold;
+}
+*{
+    box-sizing:border-box;
+}
+a {
+	text-decoration:none;
+}
+button{
+	cursor:pointer;
+}
+`;
+export default GlobalStyle;
+
+export { GlobalStyle };
--- src/componentes/App.js
+++ src/componentes/App.js
@@ -2,16 +2,16 @@ import styled from "styled-components";
 import { GlobalStyle } from "../assets/globalStyles";
 
 export default function App() {
-    return (
-        <>  
-            <GlobalStyle />
-            <Titulo> HEllo World </Titulo>
-        </>
-    )
+  return (
+    <>
+      <GlobalStyle />
+      <Titulo> HEllo World </Titulo>
+    </>
+  );
 }
 
 const Titulo = styled.h1`
-    font-size: 25px;
-    font-weight: 700;
-    color: red;
-`
+  font-size: 25px;
+  font-weight: 700;
+  color: red;
+`;
