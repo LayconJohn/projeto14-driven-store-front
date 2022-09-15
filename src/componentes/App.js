5b81624de80618251c76871b08ffabe87a96f919
Norival <norivaljunior.jf@gmail.com>
2022-09-14 21:23:49 -0300

    iniciando produção da pagina de produtos

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,7 +1,11 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Produtos from "./Produtos/Produtos";
 export default function App() {
-    return (
-        <>
-            Hello World
-        </>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Produtos />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- src/componentes/Produtos/Produtos.js
+++ src/componentes/Produtos/Produtos.js
@@ -1,3 +1,51 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
 export default function Produtos() {
-  return <></>;
+  const [produtos, setProdutos] = useState();
+  useEffect(() => {
+    const promise = axios.get(`http://localhost:5000/produtos`);
+    promise.then((res) => setProdutos(res.data));
+  }, []);
+
+  return (
+    <Container>
+      {produtos?.map((res) => (
+        <Item>
+          <img src={res.image} />
+          <p>{res.title}</p>
+          <p>{res.price}</p>
+        </Item>
+      ))}
+    </Container>
+  );
 }
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  margin: 0 auto;
+  background-color: #f4f4f4;
+  display: flex;
+  flex-wrap: wrap;
+`;
+const Item = styled.div`
+  width: 150px;
+  height: 200px;
+  margin: 20px 20px;
+  padding: 5px;
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  img {
+    max-width: 100px;
+    max-height: 100px;
+    width: auto;
+    height: auto;
+  }
+  p {
+    text-align: center;
+  }
+`;
